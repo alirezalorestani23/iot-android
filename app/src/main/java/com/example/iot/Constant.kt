40f0a3f8aa05e16129f2c82bc4b28036e0a81edd
alirezalorestani <alirezalorestani2010@gmail.com>
2021-07-05 17:21:58 +0430

    #5 -add polygon in map

---
+++ app/src/main/java/com/example/iot/Constant.kt
@@ -0,0 +1,25 @@
+package com.example.iot
+
+import org.neshan.common.model.LatLng
+
+object Constant {
+    val universityArea = arrayListOf(
+        LatLng(36.301853, 59.536659),
+        LatLng(36.301660, 59.535231),
+        LatLng(36.301190, 59.535254),
+        LatLng(36.299394, 59.524093),
+        LatLng(36.303026, 59.517280),
+        LatLng(36.307362, 59.514389),
+        LatLng(36.310330, 59.515907),
+        LatLng(36.313136, 59.519476),
+        LatLng(36.312315, 59.522244),
+        LatLng(36.308018, 59.517618),
+        LatLng(36.307292, 59.514796),
+        LatLng(36.307015, 59.515027),
+        LatLng(36.307453, 59.518027),
+        LatLng(36.307453, 59.518027),
+        LatLng(36.318796, 59.527684),
+        LatLng(36.315045, 59.539581),
+        LatLng(36.314079, 59.539959)
+    )
+}
--- app/src/main/java/com/example/iot/HomeFragment.kt
+++ app/src/main/java/com/example/iot/HomeFragment.kt
@@ -1,10 +1,19 @@
 package com.example.iot
 
+import com.carto.graphics.Color
+import com.carto.styles.LineStyle
+import com.carto.styles.LineStyleBuilder
+import com.carto.styles.PolygonStyle
+import com.carto.styles.PolygonStyleBuilder
+import com.example.iot.Constant.universityArea
 import com.example.iot.databinding.FragmentHomeBinding
 import org.neshan.common.model.LatLng
+import org.neshan.mapsdk.MapView
+import org.neshan.mapsdk.model.Polygon
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
+    private var polygon: Polygon? = null
     override fun onViewCreated() {
         initMap()
     }
@@ -12,11 +21,50 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     private fun initMap() {
         dataBinding?.let {
             it.map.apply {
-                moveCamera(LatLng(57.7510, -97.8220), 0f)
+                drawPolygon(this)
+                moveCamera(LatLng(36.315846, 59.530222), 0f)
                 setZoom(14f, 0f)
                 isTrafficEnabled = true
             }
         }
     }
 
+    private fun drawPolygon(map: MapView) {
+        removeLastPolygon()
+        polygon = Polygon(universityArea, getPolygonStyle())
+        map.addPolygon(polygon)
+    }
+
+    private fun removeLastPolygon() {
+        if (polygon != null) {
+            dataBinding?.map?.removePolygon(polygon)
+        }
+    }
+
+    private fun getPolygonStyle(): PolygonStyle {
+        val polygonStCr = PolygonStyleBuilder()
+        polygonStCr.lineStyle = getLineStyle()
+        polygonStCr.color = Color(0)
+        return polygonStCr.buildStyle()
+    }
+
+    private fun getLineStyle(): LineStyle {
+        val lineStCr = LineStyleBuilder()
+        lineStCr.color = Color(2.toShort(), 119.toShort(), 189.toShort(), 190.toShort())
+        lineStCr.width = 2f
+        lineStCr.stretchFactor = 1f
+        return lineStCr.buildStyle()
+    }
 }
+
+/**
+ *
+ * 36.315846, 59.530222
+ * 36.313339, 59.529253
+ * 36.310954, 59.533420
+ * 36.307406, 59.520140
+ * 36.303639, 59.530528
+ *
+ *
+ *
+ * */
